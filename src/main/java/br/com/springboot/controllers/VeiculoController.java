c1353e14c248d1e763dbb864f45022fb232ecb5f
Dyuliano Cecilio <dyuliano_cecilio@hotmail.com>
2021-11-05 16:09:04 -0300

    Controller veiculo

---
+++ src/main/java/br/com/springboot/controllers/VeiculoController.java
@@ -0,0 +1,45 @@
+package br.com.springboot.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.model.Veiculo;
+import br.com.springboot.repository.VeiculoRepository;
+
+@RestController
+@RequestMapping("/veiculos")
+public class VeiculoController {
+
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    @GetMapping("/{id}")
+    public Veiculo user(@PathVariable("id") Long id) {
+        System.out.println(String.format("O id é: %s", id));
+        Optional<Veiculo> veiculosFind = this.veiculoRepository.findById(id);
+
+        if (veiculosFind.isPresent()) {
+            return veiculosFind.get();
+        }
+        return null;
+    }
+
+    @PostMapping("")
+    public Veiculo veiculo(@RequestBody Veiculo veiculo) {
+        return this.veiculoRepository.save(veiculo);
+    }
+
+    @GetMapping("/list")
+    public List<Veiculo> list() {
+        return this.veiculoRepository.findAll();
+    }
+
+}
