117a870d3e9d3900116e9d9a213a5825094804ed
Dyuliano Cecilio <dyuliano_cecilio@hotmail.com>
2021-11-05 23:14:08 -0300

    Limpeza do projeto

--- src/main/java/br/com/springboot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.springboot.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
